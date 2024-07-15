61496d8761af0e7209c2ddd340f69725fe223d61
MI Sajid <misajidsa@gmail.com>
2024-07-15 22:40:08 +0600

    (chore) add next-ui plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,36 @@
+import { nextui } from "@nextui-org/react"
 import type { Config } from "tailwindcss"
 
-const config: Config = {
-    content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
-    theme: {},
-    plugins: [require("tailwindcss-fluid-type")],
-}
+const config = {
+    darkMode: ["class"],
+    content: ["./src/**/*.{ts,tsx}", "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
+    prefix: "",
+    theme: {
+        container: {
+            center: true,
+            padding: "2rem",
+            screens: {
+                "2xl": "1400px",
+            },
+        },
+        extend: {
+            keyframes: {
+                "accordion-down": {
+                    from: { height: "0" },
+                    to: { height: "var(--radix-accordion-content-height)" },
+                },
+                "accordion-up": {
+                    from: { height: "var(--radix-accordion-content-height)" },
+                    to: { height: "0" },
+                },
+            },
+            animation: {
+                "accordion-down": "accordion-down 0.2s ease-out",
+                "accordion-up": "accordion-up 0.2s ease-out",
+            },
+        },
+    },
+    plugins: [require("tailwindcss-animate"), nextui()],
+} satisfies Config
 
 export default config
