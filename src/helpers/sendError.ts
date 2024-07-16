a91c4f9075a64e2e6b112cbecf40db49c298bacb
MI Sajid <misajidsa@gmail.com>
2024-07-16 20:40:13 +0600

    feat: add support for error handler

--- src/helpers/sendError.ts
+++ src/helpers/sendError.ts
@@ -1,5 +1,8 @@
-export function sendError(e: unknown) {
+export function sendError(e: unknown, errHandler?: (err: Error) => string | undefined): { success: false; message: string } {
     if (typeof e === "string") return { success: false, message: e }
-    if (e instanceof Error) return { success: false, message: e.message }
+    if (e instanceof Error) {
+        const message = errHandler && errHandler(e)
+        return { success: false, message: message || "Something wen't wrong" }
+    }
     return { success: false, message: "Something wen't wrong" }
 }
