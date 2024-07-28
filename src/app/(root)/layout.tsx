3355980f47190a1a288aec48f7cd6621ed93a31b
MI Sajid <misajidsa@gmail.com>
2024-07-28 15:29:19 +0600

    feat: add root layout

--- src/app/(root)/layout.tsx
+++
@@ -1,5 +0,0 @@
-import RootLayout from "@/components/RootLayout"
-
-export default async function layout({ children }: { children: React.ReactNode }) {
-    return <RootLayout>{children}</RootLayout>
-}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import RootLayout from "@/components/RootLayout"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -16,7 +17,9 @@ export default function layout({
 }>) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <RootLayout>{children}</RootLayout>
+            </body>
         </html>
     )
 }
