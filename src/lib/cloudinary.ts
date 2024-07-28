d3e50424d354db4bb01a39445fc1aca882fd56ef
MI Sajid <misajidsa@gmail.com>
2024-07-28 15:20:12 +0600

    feat: export cloudinary config

---
+++ src/lib/cloudinary.ts
@@ -0,0 +1,10 @@
+import cloudinary from "cloudinary"
+
+cloudinary.v2.config({
+    cloud_name: "ddrosdazm",
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true,
+})
+
+export { cloudinary }
