5563723f9329390bfeb134dcf689b97d68cd6e08
MI Sajid <misajidsa@gmail.com>
2024-08-03 04:48:35 +0600

    feat: add root layout provider

---
+++ src/components/RootLayoutProvider.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { store } from "@/store/store"
+import { user } from "@prisma/client"
+import { useEffect } from "react"
+import ErrorComponent from "./ErrorComponent"
+import { ErrorBoundary } from "react-error-boundary"
+
+export default function RootLayoutProvider({ children, user }: { children: React.ReactNode; user?: Pick<user, "id" | "name" | "email" | "avatar_url"> }) {
+    useEffect(() => {
+        store.user = user
+    }, [user])
+
+    return <ErrorBoundary FallbackComponent={ErrorComponent}>{children}</ErrorBoundary>
+}
