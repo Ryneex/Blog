dbdf1d78a57d081421056ce091bf9ea9440523de
MI Sajid <misajidsa@gmail.com>
2024-08-02 19:40:43 +0600

    feat: add blog validation

---
+++ src/validations/blog.ts
@@ -0,0 +1,22 @@
+import { z } from "zod"
+
+export const blogValidation = z.object({
+    cover: z.custom<string>((e) => {
+        const isUrl = z.string().url().trim().safeParse(e)
+        if (isUrl.success) return isUrl.data
+        if (e instanceof File) {
+            if (["image/png", "image/jpg", "image/jpeg"].includes(e.type)) return e
+        }
+        return false
+    }, "Invalid cover image"),
+    title: z.string().trim().min(10, "title must be more than 10 chars").max(50, "title must be less than 50 chars"),
+    description: z.string().trim().min(50, "description must be more than 10 chars").max(1000, "description must be less than 1000 chars"),
+    content: z.string().refine((value) => {
+        try {
+            JSON.parse(value)
+            return true
+        } catch (_) {
+            return false
+        }
+    }),
+})
