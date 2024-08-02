d8660d34486dd7b9ed5e2dca9234d1f6654b6f9f
MI Sajid <misajidsa@gmail.com>
2024-08-03 03:56:16 +0600

    feat: add helper function to get cover url

---
+++ src/helpers/getCoverUrl.ts
@@ -0,0 +1,21 @@
+import { cloudinary } from "@/lib/cloudinary"
+import { sendError } from "./sendError"
+import { ErrorResponse, SuccessResponse } from "@/types"
+
+type ITypeGetCoverUrl = ErrorResponse | SuccessResponse<{ url: string }>
+/**
+ * If the `cover` is already a URL, the function just returns it. If it's a file, the function uploads it to Cloudinary and then returns the URL.
+ */
+export async function getCoverUrl(cover: string | File): Promise<ITypeGetCoverUrl> {
+    if (typeof cover === "string") return { success: true, url: cover }
+
+    const res: ITypeGetCoverUrl = await new Promise(async (res) => {
+        const stream = cloudinary.v2.uploader.upload_stream({ resource_type: "image", folder: "/blog/cover" }, (err, data) => {
+            if (err) return res(sendError(err.message))
+            if (data) return res({ success: true, url: data.secure_url })
+            res(sendError("something went wrong"))
+        })
+        stream.end(Buffer.from(await cover.arrayBuffer()))
+    })
+    return res
+}
