0479fbdcdb16c3bfd18b4c97928667717ea3f0c2
MI Sajid <misajidsa@gmail.com>
2024-08-03 03:58:59 +0600

    fix: use native Pick type

--- src/helpers/selectKeys.ts
+++ src/helpers/selectKeys.ts
@@ -1,9 +1,5 @@
-export type ISelectKeys<T extends object, K extends keyof T> = {
-    [P in K]: T[P]
-}
-
 export function SelectKeys<T extends object, K extends keyof T>(obj: T, arr: K[]) {
-    const result = {} as ISelectKeys<T, K>
+    const result = {} as Pick<T, K>
     for (const key of arr) {
         if (key in obj) {
             result[key] = obj[key]
