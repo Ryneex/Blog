9e569540ced3d62f3b94b566ac1ba98edeb888ce
MI Sajid <misajidsa@gmail.com>
2024-08-03 04:46:44 +0600

    fix: add loading state

--- src/helpers/callActionWithToast.ts
+++ src/helpers/callActionWithToast.ts
@@ -1,12 +1,15 @@
 import { toast } from "sonner"
 
 export async function callActionWithToast<T extends object, M extends string>(promise: Promise<T>, message?: M): Promise<T> {
+    const toastId = toast.loading("Processing your request", {
+        position: "top-center",
+    })
     const response = await promise
-    const position = { position: "top-center" } as const
+    const toastOption = { position: "top-center", id: toastId } as const
     if ("success" in response && typeof response.success === "boolean") {
         if ("message" in response && typeof response.message === "string") {
-            toast[response.success ? "success" : "error"](response.message, position)
-        } else toast.success(message || "success", position)
+            toast[response.success ? "success" : "error"](response.message, toastOption)
+        } else toast.success(message || "success", toastOption)
     }
     return response
 }
