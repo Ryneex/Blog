59f5c2a07865611322053d5dc13c248d804fc49f
MI Sajid <141750743+Ryneex@users.noreply.github.com>
2024-08-10 03:22:02 +0600

    feat: use blog id as key

--- src/helpers/getCoverUrl.ts
+++ src/helpers/getCoverUrl.ts
@@ -6,11 +6,11 @@ type ITypeGetCoverUrl = ErrorResponse | SuccessResponse<{ url: string }>
 /**
  * If the `cover` is already a URL, the function just returns it. If it's a file, the function uploads it to Cloudinary and then returns the URL.
  */
-export async function getCoverUrl(cover: string | File): Promise<ITypeGetCoverUrl> {
+export async function getCoverUrl(cover: string | File, blogId: string): Promise<ITypeGetCoverUrl> {
     if (typeof cover === "string") return { success: true, url: cover }
 
     const res: ITypeGetCoverUrl = await new Promise(async (res) => {
-        const stream = cloudinary.v2.uploader.upload_stream({ resource_type: "image", folder: "/blog/cover" }, (err, data) => {
+        const stream = cloudinary.v2.uploader.upload_stream({ resource_type: "image", folder: "/blog/cover", public_id: blogId }, (err, data) => {
             if (err) return res(sendError(err.message))
             if (data) return res({ success: true, url: data.secure_url })
             res(sendError("something went wrong"))
