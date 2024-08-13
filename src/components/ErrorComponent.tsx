270f87f1a0d17a982af5de40d78ff6365b5119f0
MI Sajid <141750743+Ryneex@users.noreply.github.com>
2024-08-13 22:18:12 +0600

    fix: remove link import

--- src/components/ErrorComponent.tsx
+++ src/components/ErrorComponent.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { Button } from "@nextui-org/react"
-import Link from "next/link"
 import { useRouter } from "next/navigation"
 
 export default function ErrorComponent({ error, resetErrorBoundary }: { error: Error; resetErrorBoundary(): void }) {
