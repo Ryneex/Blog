f0bcabc0c097bef182f9dc212d921b40043e8db3
MI Sajid <141750743+Ryneex@users.noreply.github.com>
2024-08-13 18:42:39 +0600

    feat: add update user validation

--- src/validations/user.ts
+++ src/validations/user.ts
@@ -10,3 +10,8 @@ export const signupValidation = z.object({
     email: z.string().toLowerCase().trim().email(),
     password: z.string().min(5).max(100),
 })
+
+export const updateUserValidation = z.object({
+    name: z.string().trim().min(5).max(100),
+    bio: z.string().trim().min(10).max(200).nullable(),
+})
