5f1a7c4e8eb37efe379573c1cedd31062537cf7a
MI Sajid <141750743+Ryneex@users.noreply.github.com>
2024-08-13 20:04:25 +0600

    feat: use next-ui popover

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,77 +1,86 @@
 "use client"
 
-import { Avatar, Button, Divider, Input, Listbox, ListboxItem, User } from "@nextui-org/react"
-import { user } from "@prisma/client"
+import { Avatar, Button, Divider, Input, Listbox, ListboxItem, useDisclosure, User } from "@nextui-org/react"
 import Link from "next/link"
 import { FaPlus, FaSearch } from "react-icons/fa"
 import { RiBloggerLine } from "react-icons/ri"
 import { MdLogout } from "react-icons/md"
 import { FiUser } from "react-icons/fi"
-import { logout } from "@/actions/user/logout"
+import { logout } from "@/actions/user/auth/logout"
 import { useRouter } from "next/navigation"
-import { Popover, PopoverContent, PopoverTrigger } from "./shadcn/ui/popover"
+import { Popover, PopoverContent, PopoverTrigger } from "@nextui-org/popover"
 import { GearIcon } from "@radix-ui/react-icons"
+import { IPrivateUser } from "@/types/user"
 
-export default function Navbar({ user }: { user?: Pick<user, "name" | "avatar_url" | "email"> }) {
-    const router = useRouter()
+export default function Navbar({ user }: { user?: IPrivateUser }) {
     return (
-        <nav className="flex h-14 shrink-0 items-center justify-between bg-white px-5">
-            <div className="flex gap-5">
-                <Link href="/" className="flex items-center gap-1 text-lg font-medium text-blue-800">
-                    <RiBloggerLine size={25} />
-                    Blog
-                </Link>
-                <Input type="text" placeholder="Search..." labelPlacement="outside" size="sm" startContent={<FaSearch className="mx-1" />} />
-            </div>
-            {!user ? (
-                <div className="flex gap-3">
-                    <Link href="/signin">
-                        <Button className="h-9 rounded-full" variant="flat">
-                            Sign in
-                        </Button>
+        <div className="shrink-0 border-b border-slate-300">
+            <nav className="container flex h-16 items-center justify-between">
+                <div className="flex gap-5">
+                    <Link href="/" className="flex items-center gap-1 text-xl font-medium text-blue-800">
+                        <RiBloggerLine size={28} />
+                        Blog
                     </Link>
-                    <Link href="/signup">
-                        <Button className="h-9 rounded-full" color="primary">
-                            Sign up
+                    <Input type="text" placeholder="Search..." labelPlacement="outside" size="sm" startContent={<FaSearch className="mx-1" />} />
+                </div>
+                {!user ? (
+                    <div className="flex gap-3">
+                        <Link href="/signin">
+                            <Button className="h-9 rounded-full" variant="flat">
+                                Sign in
+                            </Button>
+                        </Link>
+                        <Link href="/signup">
+                            <Button className="h-9 rounded-full" color="primary">
+                                Sign up
+                            </Button>
+                        </Link>
+                    </div>
+                ) : (
+                    <div className="flex items-center gap-3">
+                        <Button as={Link} href="/create" startContent={<FaPlus />} className="h-9 rounded-full" color="primary">
+                            Create
                         </Button>
-                    </Link>
-                </div>
-            ) : (
-                <div className="flex items-center gap-3">
-                    <Button as={Link} href="/create" startContent={<FaPlus />} className="h-9 rounded-full" color="primary">
-                        Create
-                    </Button>
-                    <Popover>
-                        <PopoverTrigger asChild>
-                            <Avatar isBordered className="!size-8 cursor-pointer" src={user.avatar_url ? user.avatar_url : undefined} name={user.name} />
-                        </PopoverTrigger>
-                        <PopoverContent align="end">
-                            <User name={user.name} description={user.email} avatarProps={{ src: user.avatar_url ? user.avatar_url : undefined }} />
-                            <Divider className="my-4" />
-                            <Listbox aria-label="Actions" variant="flat">
-                                <ListboxItem startContent={<FiUser className="mb-0.5" />} key="Profile">
-                                    Your Profile
-                                </ListboxItem>
-                                <ListboxItem showDivider startContent={<GearIcon className="mb-0.5" />} key="Settings">
-                                    Settings
-                                </ListboxItem>
-                                <ListboxItem
-                                    onClick={async () => {
-                                        await logout()
-                                        router.refresh()
-                                    }}
-                                    startContent={<MdLogout className="mb-0.5" />}
-                                    className="text-red-500"
-                                    color="danger"
-                                    key="Logout"
-                                >
-                                    Logout
-                                </ListboxItem>
-                            </Listbox>
-                        </PopoverContent>
-                    </Popover>
-                </div>
-            )}
-        </nav>
+                        <ProfilePopover user={user} />
+                    </div>
+                )}
+            </nav>
+        </div>
+    )
+}
+
+function ProfilePopover({ user }: { user: IPrivateUser }) {
+    const router = useRouter()
+    const { isOpen, onClose, onOpenChange } = useDisclosure()
+    return (
+        <Popover isOpen={isOpen} onOpenChange={onOpenChange} classNames={{ base: "sm:w-72" }} placement="bottom-end">
+            <PopoverTrigger>
+                <Avatar isBordered className="!size-8 cursor-pointer" src={user.avatar_url ? user.avatar_url : undefined} name={user.name} />
+            </PopoverTrigger>
+            <PopoverContent className="p-3">
+                <User className="mr-auto mt-1" name={user.name} description={user.email} avatarProps={{ src: user.avatar_url ? user.avatar_url : undefined }} />
+                <Divider className="my-4" />
+                <Listbox aria-label="Actions" variant="flat">
+                    <ListboxItem startContent={<FiUser className="mb-0.5" />} key="Profile">
+                        Your Profile
+                    </ListboxItem>
+                    <ListboxItem onClick={onClose} href="/settings" as={Link} showDivider startContent={<GearIcon className="mb-0.5" />} key="Settings">
+                        Settings
+                    </ListboxItem>
+                    <ListboxItem
+                        onClick={async () => {
+                            await logout()
+                            router.refresh()
+                        }}
+                        startContent={<MdLogout className="mb-0.5" />}
+                        className="text-red-500"
+                        color="danger"
+                        key="Logout"
+                    >
+                        Logout
+                    </ListboxItem>
+                </Listbox>
+            </PopoverContent>
+        </Popover>
     )
 }
