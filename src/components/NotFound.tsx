a1afab692231afb5e28dfd9771894eb373638a41
MI Sajid <141750743+Ryneex@users.noreply.github.com>
2024-08-15 21:15:41 +0600

    feat: make customizable

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,26 +1,24 @@
 "use client"
 import Link from "next/link"
 import { useRouter } from "next/navigation"
+import { cn } from "./shadcn/utils"
 
-export function NotFound() {
+export function NotFound({ className, title, description, hideButtons }: { className?: string; title?: string; description?: string; hideButtons?: boolean }) {
     const router = useRouter()
     return (
-        <section className="h-full bg-white dark:bg-gray-900">
-            <div className="container mx-auto flex min-h-screen items-center px-6 py-12">
-                <div className="mx-auto flex max-w-sm flex-col items-center text-center">
-                    <p className="rounded-full bg-blue-50 p-3 text-sm font-medium text-blue-500 dark:bg-gray-800">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="h-6 w-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
-                        </svg>
-                    </p>
-                    <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">Page not found</h1>
-                    <p className="mt-4 text-gray-500 dark:text-gray-400">The page you are looking for doesn&apos;t exist. Here are some helpful links:</p>
-
+        <section className={cn("grid h-full place-items-center bg-white dark:bg-gray-900", className)}>
+            <div className="flex max-w-sm flex-col items-center text-center">
+                <p className="rounded-full bg-blue-50 p-3 text-sm font-medium text-blue-500 dark:bg-gray-800">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="h-6 w-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
+                    </svg>
+                </p>
+                <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">{title ?? "Page not found"}</h1>
+                <p className="mt-4 text-gray-500 dark:text-gray-400">{description ?? "The page you are looking for doesn't exist. Here are some helpful links"}</p>
+                {!hideButtons && (
                     <div className="mt-6 flex w-full shrink-0 items-center gap-x-3 sm:w-auto">
                         <button
-                            onClick={() => {
-                                router.back()
-                            }}
+                            onClick={() => router.back()}
                             className="flex w-1/2 items-center justify-center gap-x-2 rounded-lg border bg-white px-5 py-2 text-sm text-gray-700 transition-colors duration-200 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-200 dark:hover:bg-gray-800 sm:w-auto"
                         >
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-5 w-5 rtl:rotate-180">
@@ -36,7 +34,7 @@ export function NotFound() {
                             Take me home
                         </Link>
                     </div>
-                </div>
+                )}
             </div>
         </section>
     )
