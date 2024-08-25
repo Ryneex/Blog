ee8bb92cf86af4a0f36c3dc006002413fe76c5a6
MI Sajid <141750743+Ryneex@users.noreply.github.com>
2024-08-26 00:15:51 +0600

    fix: filter out password

--- src/helpers/getCurrentUser.ts
+++ src/helpers/getCurrentUser.ts
@@ -1,5 +1,10 @@
 import { auth } from "@/lib/auth"
 import { cache } from "react"
+import { ExcludeKeys } from "./excludeKeys"
 
 // caching the function to use it in multiple components withouts refetching user info
-export const getCurrentUser = cache(auth.getCurrentUser)
+export const getCurrentUser = cache(async () => {
+    const res = await auth.getCurrentUser()
+    if (res.success) return { success: true as const, user: { ...ExcludeKeys(res.user, ["password"]) } }
+    return res
+})
