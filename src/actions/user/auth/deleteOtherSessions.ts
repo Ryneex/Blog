2dc7799576e525099206ad55116864d0ad01e8d4
MI Sajid <141750743+Ryneex@users.noreply.github.com>
2024-08-25 23:08:45 +0600

    feat: delete other sessions

---
+++ src/actions/user/auth/deleteOtherSessions.ts
@@ -0,0 +1,16 @@
+"use server"
+
+import { sendError } from "@/helpers/sendError"
+import { auth } from "@/lib/auth"
+import { client } from "@/lib/prismaClient"
+
+export async function deleteOtherSessions() {
+    try {
+        const { session } = await auth.getCurrentSession()
+        if (!session) return sendError()
+        await client.session.deleteMany({ where: { userId: session.userId, NOT: [{ id: session.id }] } })
+        return { success: true }
+    } catch (error) {
+        return sendError()
+    }
+}
