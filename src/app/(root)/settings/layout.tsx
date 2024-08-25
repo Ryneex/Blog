372e70e988b465ed061eb583b7774aa644c6d131
MI Sajid <141750743+Ryneex@users.noreply.github.com>
2024-08-26 00:14:23 +0600

    feat: redirect user if not logged id

--- src/app/(root)/settings/layout.tsx
+++ src/app/(root)/settings/layout.tsx
@@ -1,6 +1,10 @@
-import Sidebar from "./Sidebar"
+import { getCurrentUser } from "@/helpers/getCurrentUser"
+import Sidebar from "@/components/root/settings/Sidebar"
+import { redirect } from "next/navigation"
 
-export default function layout({ children }: { children: React.ReactNode }) {
+export default async function layout({ children }: { children: React.ReactNode }) {
+    const res = await getCurrentUser()
+    if (!res.success) redirect("/")
     return (
         <div className="container h-full gap-10 overflow-auto py-2 sm:flex sm:py-5">
             <Sidebar />
