f5b5d7c7dd9777f9dcb63dcf4fd33144b3603e7d
MI Sajid <141750743+Ryneex@users.noreply.github.com>
2024-08-26 00:07:32 +0600

    feat: add security page

---
+++ src/app/(root)/settings/security/page.tsx
@@ -0,0 +1,14 @@
+import SecuritySettings from "@/components/root/settings/SecuritySettings"
+import { ExcludeKeys } from "@/helpers/excludeKeys"
+import { getCurrentUser } from "@/helpers/getCurrentUser"
+import { client } from "@/lib/prismaClient"
+import { cookies } from "next/headers"
+
+export default async function page() {
+    const { user } = await getCurrentUser()
+    if (!user) return
+    const sessions = await client.session.findMany({ where: { userId: user.id }, orderBy: { createdAt: "desc" } })
+    const currentSessionId = cookies().get("session_id")!.value
+    const filteredSession = sessions.map((e) => ({ ...ExcludeKeys(e, ["id"]), isCurrent: e.id === currentSessionId }))
+    return <SecuritySettings sessions={filteredSession} />
+}
