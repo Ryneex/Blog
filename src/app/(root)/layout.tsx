0d61f1e14cb63338b6fbd699d0b510acc53a5b75
MI Sajid <141750743+Ryneex@users.noreply.github.com>
2024-08-26 03:32:15 +0600

    fix: use predefined user

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,11 +1,9 @@
 import Navbar from "@/components/Navbar"
 import RootLayoutProvider from "@/components/RootLayoutProvider"
 import { getCurrentUser } from "@/helpers/getCurrentUser"
-import { SelectKeys } from "@/helpers/selectKeys"
 
 export default async function layout({ children }: { children: React.ReactNode }) {
-    const res = await getCurrentUser()
-    const user = res.success ? SelectKeys(res.user, ["id", "name", "avatar_url", "email", "bio"]) : undefined
+    const { user } = await getCurrentUser()
     return (
         <div className="flex h-screen flex-col overflow-hidden">
             <Navbar user={user} />
--- src/app/(root)/settings/page.tsx
+++ src/app/(root)/settings/page.tsx
@@ -1,9 +1,8 @@
 import AccountSettings from "@/components/root/settings/AccountSettings"
-import { ExcludeKeys } from "@/helpers/excludeKeys"
 import { getCurrentUser } from "@/helpers/getCurrentUser"
 
 export default async function page() {
     const { user } = await getCurrentUser()
     if (!user) return
-    return <AccountSettings userData={ExcludeKeys(user!, ["password"])} />
+    return <AccountSettings userData={user} />
 }
