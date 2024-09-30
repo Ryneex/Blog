19e176fb4f9c332df178ee23d9439d4e08c6980f
MI Sajid <141750743+Ryneex@users.noreply.github.com>
2024-09-30 18:21:53 +0600

    fix: turn off location lookup

--- src/helpers/getSessionInfo.ts
+++ src/helpers/getSessionInfo.ts
@@ -1,10 +1,14 @@
-import { lookup } from "@/lib/maxmind"
 import { headers } from "next/headers"
 import { userAgent } from "next/server"
 
+/**
+ * Maxminds IP database doesn't work with vercel. So location detection feature is turned off. 😭
+ */
 export function getSessionInfo(ip: string) {
-    const ipInfo = lookup.get(ip)
-    const location = ipInfo ? `${ipInfo.city?.names.en}, ${ipInfo.country?.iso_code}` : undefined
+    // const ipInfo = lookup.get(ip)
+    // const location = ipInfo ? `${ipInfo.city?.names.en}, ${ipInfo.country?.iso_code}` : undefined
+
+    const location = ip ? undefined : undefined
     const {
         browser: { name: browser, version: browser_version },
         os: { name: os },
--- src/lib/maxmind.ts
+++ src/lib/maxmind.ts
@@ -1,3 +1,7 @@
-import fs from "fs"
-import { Reader, CityResponse } from "maxmind"
-export const lookup = new Reader<CityResponse>(fs.readFileSync("./src/lib/ip-city.mmdb"))
+// import fs from "fs"
+// import { Reader, CityResponse } from "maxmind"
+// export const lookup = new Reader<CityResponse>(fs.readFileSync("./src/lib/ip-city.mmdb"))
+
+/**
+ * Reading local database files isn't supported on vercel. so IP lookups won't work
+ */
