01d810d2e6bc7ae0396d8ea9bd7c2a5948852b49
Anas Siddiqui <anassiddiqui0910@gmail.com>
2024-10-18 18:01:40 +0500

    fix: show edit button only for profile owner

--- apps/www/src/lib/cloudinary.ts
+++ apps/www/src/lib/cloudinary.ts
@@ -1,7 +1,7 @@
 import cloudinary from "cloudinary"
 
 cloudinary.v2.config({
-    cloud_name: "ddrosdazm",
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "ddrosdazm",
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
     secure: true,
