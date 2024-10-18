634d29fb387fed25addb3d0780e8a3547e212d58
Anas Siddiqui <anassiddiqui0910@gmail.com>
2024-10-19 03:10:07 +0500

    feat: add loading for routes.

--- apps/www/src/app/(root)/layout.tsx
+++ apps/www/src/app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/root/Navbar"
 import RootLayoutProvider from "@/components/root/RootLayoutProvider"
 import { getCurrentUser } from "@/helpers/getCurrentUser"
+import NextTopLoader from "nextjs-toploader"
 
 export default async function layout({ children }: { children: React.ReactNode }) {
     const { user } = await getCurrentUser()
@@ -8,6 +9,7 @@ export default async function layout({ children }: { children: React.ReactNode }
         <div className="flex h-screen flex-col overflow-hidden bg-gray-50">
             <Navbar user={user} />
             <RootLayoutProvider user={user}>{children}</RootLayoutProvider>
+            <NextTopLoader showSpinner={false} />
         </div>
     )
 }
