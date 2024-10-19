b9cbfd396711c8505ebafbe049ab8593172bed76
Anas Siddiqui <anassiddiqui0910@gmail.com>
2024-10-19 21:04:39 +0500

    feat: added loader component

--- apps/www/src/app/(root)/layout.tsx
+++ apps/www/src/app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/root/Navbar"
+import ProgressBar from "@/components/root/ProgressBar"
 import RootLayoutProvider from "@/components/root/RootLayoutProvider"
 import { getCurrentUser } from "@/helpers/getCurrentUser"
 
@@ -6,6 +7,7 @@ export default async function layout({ children }: { children: React.ReactNode }
     const { user } = await getCurrentUser()
     return (
         <div className="flex h-screen flex-col overflow-hidden bg-gray-50">
+            <ProgressBar />
             <Navbar user={user} />
             <RootLayoutProvider user={user}>{children}</RootLayoutProvider>
         </div>
